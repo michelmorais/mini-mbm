ec77bb675e1d20c9af35da48d453386e9220403a
Michel morais <michel.braz.morais@gmail.com>
2025-09-07 18:24:51 -0300

    updated version

--- include/version/version.h
+++ include/version/version.h
@@ -60,8 +60,9 @@
     3.5   Frozen box 2d (2.3.2)
     3.6   Migrated to box2d (2.4.1)
     4.0   LiquidFun for box 2d, this version forces to drawback Box2D to 2.3.0.
+    4.1   LiquidFun for box 2d 2.3.0 and box 2d 2.4.1 in library separated
 */
-#define MBM_VERSION "4.0"
+#define MBM_VERSION "4.1"
 #endif
 
 #endif
