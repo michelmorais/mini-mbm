7aa5989f2cad8afc1dea9960516bce0cf321de6a
Michel Braz de Morais <michel.braz.morais@gmail.com>
2026-03-08 14:10:12 -0300

    fix found in line during metal implementation

--- src/test-lib/my-scene-test.h
+++ src/test-lib/my-scene-test.h
@@ -48,6 +48,7 @@ class MY_SCENE : public mbm::SCENE
     mbm::SHAPE_MESH* shape;
     mbm::LINE_MESH* line;
     mbm::PARTICLE* particle;
+    mbm::PARTICLE* particle_ptl;
     mbm::RENDER_2_TEXTURE* render2Texture;
     mbm::RENDERIZABLE* toTrack;
 	mbm::STEERED_PARTICLE* steeredParticle;
@@ -56,7 +57,11 @@ class MY_SCENE : public mbm::SCENE
 	mbm::HMD* hmd;
 	mbm::TILE* tile;
     mbm::TEXTURE_VIEW* texture;
+    mbm::TEXT_DRAW * mousePositionText;
     bool randomizeParticleEachLoop;
+    float backGroundTimeToHide;
+    int windowWidth;
+    int windowHeight;
 
 	void randomSteeredParticlePositions();
     MY_SCENE();
