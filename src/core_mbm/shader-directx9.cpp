81b572ee94472252e939b4f21c3ad79bc7579213
Michel Braz de Morais <michel.braz.morais@gmail.com>
2026-05-09 10:00:58 -0300

    removed some logs no needed

--- src/core_mbm/shader-directx9.cpp
+++ src/core_mbm/shader-directx9.cpp
@@ -749,15 +749,6 @@ namespace mbm
         const int sizeOfCodeVS = strlen(codeVS);
 #if defined _DEBUG
         constexpr DWORD flag = D3DXSHADER_DEBUG;
-        if (ptrPshader)
-        {
-            static std::unordered_set<std::string> pixel_used;
-            auto it = pixel_used.insert(ptrPshader->fileName);
-            if (it.second)
-            {
-                INFO_AT(__LINE__, __FILE__, "Pixel Shader used %s", ptrPshader->fileName.c_str());
-            }
-        }
 #else
         constexpr DWORD flag = D3DXSHADER_SKIPVALIDATION;
 #endif
