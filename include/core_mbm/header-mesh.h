677a9f432f57a37b5d1901ed4753ef9db6ed0cac
Michel Braz de Morais <michel.braz.morais@gmail.com>
2026-06-23 07:34:16 -0300

    fix Windows

--- include/core_mbm/header-mesh.h
+++ include/core_mbm/header-mesh.h
@@ -491,14 +491,14 @@ namespace util
         ~INFO_SHADER_DATA()noexcept;
     };
 
-    struct API_IMPL INFO_FX
+    struct INFO_FX
     {
         INFO_SHADER_DATA *dataPS;          // Data do pixel Shader
         INFO_SHADER_DATA *dataVS;          // Data do vertex Shader
         char *             fileNameTextureAnimationEffect; // Canonical animation-level FX texture path
         int                blendOperation;  // Tipo de operacao blend nos steps
-        INFO_FX()noexcept;
-        ~INFO_FX()noexcept;
+        API_IMPL INFO_FX()noexcept;
+        API_IMPL ~INFO_FX()noexcept;
         API_IMPL void setTextureAnimationEffectFileName(const char *fileName) noexcept;
         API_IMPL const char *getTextureAnimationEffectFileName() const noexcept;
         API_IMPL bool normalizeLegacyTextureAnimationEffectPaths() noexcept;
