8dae9fd25c4f77e9eedc28fb50b5d1745dc46372
Michel Braz de Morais <michel.braz.morais@gmail.com>
2026-07-22 18:26:08 -0300

    DEBUG

--- src/core_mbm/texture-manager.cpp
+++ src/core_mbm/texture-manager.cpp
@@ -1016,7 +1016,7 @@ namespace mbm
                 constexpr int sizeFilters = sizeof(filters) / sizeof(char*);
                 std::string where_str("where:");
                 where_str += fileName;
-                #if defined (_DEBUG)
+                #if defined (_DEBUG) || defined (DEBUG)
                     printf("file not found: %s, launching file dialog. this block the application!\n If you are running in a headless environment, you can undefine USE_TEXTURE_MISSING_DIALOG to disable the file dialog.", where_str.c_str());
                 #endif
 
